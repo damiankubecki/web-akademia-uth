184c211400a008693c2b4f941d401a59f6964ddd
Damian Kubecki <damian_094@wp.pl>
2025-06-07 21:37:05 +0200

    update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then(m => m.HomeComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then(m => m.AboutComponent),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact.component').then(m => m.ContactComponent),
+  },
+  {
+    path: 'courses',
+    loadComponent: () =>
+      import('./pages/courses/courses.component').then(m => m.CoursesComponent),
+  },
+];
