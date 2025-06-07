0e2cb2813cd150d98a5839a2e43a42e245fa98a0
Damian Kubecki <damian_094@wp.pl>
2025-06-07 21:36:54 +0200

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
 
 @Component({
   selector: 'web-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
-    <h1>Welcome to {{ title }}!</h1>
-
-    <router-outlet />
+    <div class="wrapper">
+      <web-header />
+      <main>
+        <router-outlet />
+      </main>
+      <web-footer />
+    </div>
   `,
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'web-akademia';
-}
+export class AppComponent {}
